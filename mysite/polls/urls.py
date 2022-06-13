8d4486110fc741f6eeea80f7f995d6c7d715298d
Sebastian <sebaslogo@gmail.com>
2022-06-13 18:14:48 -0500

    Added forms for completions

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("create_user/",views.create_user, name="create_user"),
     path("user/<int:unique_id>/",views.user,name="user"),
     path("create_bounty/",views.create_bounty, name="create_bounty"),
-    path("bounty/<int:unique_id>/",views.bounty,name="bounty")
+    path("bounty/<int:unique_id>/",views.bounty,name="bounty"),
+    path("create_completion/",views.create_completion, name="create_completion"),
+    path("completion/<int:unique_id>/",views.completion,name="completion")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from .forms import UserForm, BountyForm, CompletionForm
-from .models import User
+from .models import User, Bounty, Completion
 
 
 def index(request):
     return HttpResponse("Hello")
 
+# USER
 def create_user(request):
     context = {}
     form = UserForm(request.POST or None, request.FILES or None)
@@ -23,6 +24,7 @@ def user(request,unique_id):
 
     return HttpResponse(User.objects.get(pk=unique_id))
 
+# BOUNTY
 def create_bounty(request):
     context = {}
     form = BountyForm(request.POST or None, request.FILES or None)
@@ -37,4 +39,20 @@ def create_bounty(request):
 def bounty(request,unique_id):
 
     return HttpResponse(Bounty.objects.get(pk=unique_id))
+
+# COMPLETION
+def create_completion(request):
+    context = {}
+    form = CompletionForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        new_completion = form.save()
+        return HttpResponseRedirect(f"/polls/completion/{new_completion.pk}/")
+
+    context["form"] = form
+    return render(request,"polls/create_completion.html",context) # need a template for this
+
+def completion(request,unique_id):
+
+    return HttpResponse(Completion.objects.get(pk=unique_id))
 
