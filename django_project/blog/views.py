4e300cc5caaadb17b9928943d1da25885819767f
Sebastian <sebaslogo@gmail.com>
2022-06-29 17:07:15 -0500

    Tutorial completed

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
@@ -15,7 +16,18 @@ class PostListView(ListView):
     template_name = "blog/home.html" # <app>/<model>_<viewtype>.html
     context_object_name = "posts"
     ordering = ["-date_posted"]
+    paginate_by = 10
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html" # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User,username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
+    
 class PostDetailView(DetailView):
     model = Post
 
