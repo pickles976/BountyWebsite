78f84b25e3d13451a322902ce9165c68ccd1a744
unknown <sebaslogo@gmail.com>
2022-07-24 14:13:52 -0500

    Added Messages to models. Code freeze before Discord auth

--- django_project/bounty/models.py
+++ django_project/bounty/models.py
@@ -1,5 +1,7 @@
+from cgitb import text
 from io import StringIO
 from secrets import choice
+from tkinter import CASCADE
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -176,3 +178,7 @@ class Images(models.Model):
         storage, path = self.image.storage, self.image.path
         storage.delete(path)
 
+class Message(models.Model):
+
+    user = models.ForeignKey(User,on_delete=CASCADE)
+    text = models.CharField(max_length=128)
