0259c5ba0d16926ecfbca2d4ff0970e5237ce204
unknown <sebaslogo@gmail.com>
2022-07-27 15:40:47 -0500

    Added memcachify

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -178,3 +178,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']  
 CELERY_TASK_SERIALIZER = 'json'  
 CELERY_RESULT_SERIALIZER = 'json'  
+
+# LINUX ONLY
+from memcacheify import memcacheify
+CACHES = memcacheify()
