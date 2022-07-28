2505d3cc1e76e14bb1144abd9f73cbf9e4d4a162
unknown <sebaslogo@gmail.com>
2022-07-28 18:02:56 -0500

    SEcret key needed to deploy

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+# SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY="django-insecure-08)*$!sbpr2mrh=w@w7ymq9bn#=*op6rq=f(=mzm&g-$ms(9#h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
