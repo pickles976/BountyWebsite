ac5190b4447184d19fe07e5686a074e074876df6
unknown <sebaslogo@gmail.com>
2022-07-29 16:39:39 -0500

    Added config

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -219,8 +219,26 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 # LINUX ONLY
 if DEBUG == False:
-    from memcacheify import memcacheify
-    CACHES = memcacheify()
+
+    # from memcacheify import memcacheify
+    # CACHES = memcacheify()
 
     # auto-handles Heroku configuration for PostgreSQL
-    django_heroku.settings(locals())
+    # django_heroku.settings(locals())
+
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django_db_geventpool.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '',
+        'PORT': '',
+        'ATOMIC_REQUESTS': False,
+        'CONN_MAX_AGE': 0,
+        'OPTIONS': {
+            'MAX_CONNS': 20,
+            'REUSE_CONNS': 10
+        }
+    }
+}
