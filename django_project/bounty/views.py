89d5d3720846450821b5da3e6d9cde4786e004a0
unknown <sebaslogo@gmail.com>
2022-07-29 14:46:20 -0500

    4 images max

--- django_project/bounty/views.py
+++ django_project/bounty/views.py
@@ -76,7 +76,7 @@ def postBountyView(request):
         return redirect("bounty-about")
  
     ImageFormSet = modelformset_factory(Images,
-                                        form=ImageForm, extra=5)
+                                        form=ImageForm, extra=4)
     #'extra' means the number of photos that you can upload   ^
     if request.method == 'POST':
     
