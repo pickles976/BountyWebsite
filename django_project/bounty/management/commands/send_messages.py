eea5b72a9f6bd53685e03841fbd1e97596b8e19d
unknown <sebaslogo@gmail.com>
2022-07-31 15:26:26 -0500

    Added messaging command

--- django_project/bounty/management/commands/send_messages.py
+++ django_project/bounty/management/commands/send_messages.py
@@ -1,59 +1,20 @@
 from django.core.management.base import BaseCommand
 from bounty.models import Message
-import os
-
-DISCORD_BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
-
-def send_messages(users):
-
-    import time
-    import discord
-
-    s = time.time()
-    client = discord.Client()
-
-    @client.event
-    async def on_ready():
-
-        print(f'We have logged in as {client.user}')
-        print(f"{(time.time() - s)*1000}ms elapsed")
-
-        start = time.time()
-        for user in users:
-            await send_message(user,users[user])
-        print(f"{(time.time() - start)*1000}ms elapsed")
-
-        await client.close()
-
-    async def send_message(target,payload):
-        try:
-            user = await client.fetch_user(target)
-            await user.send(payload)
-        except:
-            print("Could not message user!")
-
-    client.run(DISCORD_BOT_TOKEN)
+from users.models import Profile
+from bounty.models import Message
 
 class Command(BaseCommand):
-    help = "Gets all messages from database and sends them to users through Discord"
+    help = "Checks the Foxhole WAR API for a new war. If new war has started, all players are un-verified"
+
+    def add_arguments(self, parser):
+        parser.add_argument('message', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        
-        all_messages = Message.objects.all()
 
-        new_dict = {}
+        message = options["message"]
+        p = Profile.objects.filter(verified=True,discordmessage=True)
 
-        for message in all_messages.iterator():
-            discordid = message.user.profile.discordid
-            
-            if message.user.profile.discordmessage:
+        for profile in p.iterator():
 
-                if discordid in new_dict:
-                    new_dict[discordid] += "\n" + message.text
-                else:
-                    new_dict[discordid] = message.text
-
-        all_messages.delete()
-        
-        send_messages(new_dict)
-
+            m = Message(user=profile.user,text=message)
+            m.save()
